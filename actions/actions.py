5a8da18bb00912241e68636ecffd149aa260dc7f
Dashavod <dvodolazska@morebis.net>
2022-10-24 14:58:55 +0300

    format response in orbit question; add question with general info about planet

--- actions/actions.py
+++ actions/actions.py
@@ -31,10 +31,12 @@ class CosmoRepository:
         self.items = DBRepository("Solar_system")
     def findPlanet(self,name):
         planet = self.items.find({"Name": name})
-        return planet['Distance']
+        return planet
     def filterPlanet(self,filter):
         planets = self.items.find_many({"Orbits": filter})
         return planets
+
+
 class UserRepository:
     def __init__(self):
         self.items = DBRepository("Users")
@@ -87,7 +89,7 @@ class ActionAskCosmoQuestion(Action):
         if(planet == None):
             return dispatcher.utter_message(text=f"No planet {planet}")
         res = cosmo.findPlanet(planet)
-        dispatcher.utter_message(text=f"Radius of {planet} is {res}000km")
+        dispatcher.utter_message(text=f"Radius of {planet} is {res['Distance']}000km")
 
         return SlotSet("planet",None)
    
@@ -105,11 +107,40 @@ class ActionAskOrbitQuestion(Action):
             return dispatcher.utter_message(text=f"No planet {planet}")
         res = cosmo.filterPlanet(planet)
         names = []
-        for i in res: names.append(i["Name"])
-        dispatcher.utter_message(text=f"On orbit {planet}: {names.count} satelits")
+        for i in res:
+            names.append({'name':i['Name'],'number_rome': i['Number'],'number':romanToInt(i['Number'])})
+        names.sort(key=lambda x: x.get('number'))
+        dispatcher.utter_message(text=f"On orbit {planet}: ")
 
-        dispatcher.utter_message(text=f"{names}")
+        for i in names:dispatcher.utter_message(text=f"{i['number_rome']} {i['name']},\n")
         return {"planet": None}
+
+class ActionInfoQuestion(Action):
+
+    def name(self) -> Text:
+        return "action_info_planet"
+
+    def run(self, dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        planet = tracker.get_slot("planet")
+        if(planet == None):
+            return dispatcher.utter_message(text=f"No planet {planet}")
+        generalInfo = cosmo.findPlanet(planet)
+        res = cosmo.filterPlanet(planet)
+        names = []
+        for i in res:
+            names.append({'name':i['Name'],'number_rome': i['Number'],'number':romanToInt(i['Number'])})
+        names.sort(key=lambda x: x.get('number'))
+        dispatcher.utter_message(text=f"{ generalInfo['Name'] }:\nOrbit: {generalInfo['Orbits']} \nNumber in orbit:{generalInfo['Number']}\n")
+        dispatcher.utter_message(text=f"Radius: { generalInfo['Distance'] }000km \nPeriod: {generalInfo['O_Period']} days \nIncl:{generalInfo['Incl']}\nEccen:{generalInfo['Eccen']}\n")
+        dispatcher.utter_message(text=f"Discoverer:{ generalInfo['Discoverer'] }\nDate: {generalInfo['Date']} \nPseudonym:{generalInfo['AKA']}\n")
+        dispatcher.utter_message(text=f"On orbit {planet}:")
+
+        for i in names:dispatcher.utter_message(text=f"{i['number_rome']} {i['name']},\n")
+        return {"planet": None}
+         
 class ActionCheckQuestions(Action):
 
     def name(self) -> Text:
@@ -164,3 +195,21 @@ class ValidateNameForm(FormValidationAction):
             dispatcher.utter_message(text="That must've been a typo.")
             return {"first_name": None}
         return {"first_name": name}
+
+def romanToInt(s):
+      """
+      :type s: str
+      :rtype: int
+      """
+      roman = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000,'IV':4,'IX':9,'XL':40,'XC':90,'CD':400,'CM':900}
+      i = 0
+      num = 0
+      while i < len(s):
+         if i+1<len(s) and s[i:i+2] in roman:
+            num+=roman[s[i:i+2]]
+            i+=2
+         else:
+            #print(i)
+            num+=roman[s[i]]
+            i+=1
+      return num
--- test.py
+++ test.py
@@ -11,42 +11,28 @@ class DBRepository:
     def insert(self,param):
         self.table.insert_one(param)
     def find(self,filter):
+        return self.table.find_one(filter)
+    def update(self,filter,param):
+        return self.table.update_one(filter,param) 
+    def find_many(self,filter):
         items = self.table.find(filter)
         return list(items)
-    def update(self,filter,param):
-        return self.table.update_one(filter,param)   
-#client = MongoClient("mongodb+srv://root:nMoiWNI9fZAvAEf2@cluster0.hif69ym.mongodb.net/?retryWrites=true&w=majority")
-#db = client.get_database('Train_Bot')
-#users = db.Users
-#user = users.update_one({"name": "dasha"}, {'$push':{ 'questions': "lalala" }})
-class BaseRepository:
-    def __init__(self):
-        self.items = DBRepository("Users")
-    def findUser(self,name):
-        text = "Ok, i remember"
-        if(self.items.find({'name': name})):
-            text = "Hello glad to see you again"
-        else:
-            self.items.insert({'name': name, 'questions': []})
-        return text
-    def addQuestions(self,name, question):
-        self.items.update({"name": name}, {'$push':{ 'questions': question }})
-
-    def findQuestions(self,name):
-        user = self.items.find({"name": name})
-        return list(user['questions'])
 
 class CosmoRepository:
     def __init__(self):
         self.items = DBRepository("Solar_system")
     def findPlanet(self,name):
-        planet = self.items.find({"Orbits": name})
+        planet = self.items.find({"Name": name})
         return planet
-
+    def filterPlanet(self,filter):
+        planets = self.items.find_many({"Orbits": filter})
+        return planets
 cosmo = CosmoRepository()
 
-res = cosmo.findPlanet("Mars")
-print(res[0]["Name"])
+generalInfo = cosmo.findPlanet("Mars")
+res = cosmo.filterPlanet("Mars")
+print(f"Radius of Mars is {generalInfo['Distance']}000km")
+print(f"Mars is {generalInfo}")
 names = []
 for i in res: names.append(i["Name"])
 print(names)
